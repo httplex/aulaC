47b0fe8320de0a0c3c28d4a1be38fdf7c82d8e7e
Alex Silva <106414021+httplex@users.noreply.github.com>
2023-09-01 03:01:59 +0000

    ok

---
+++ listaC/ex4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int quadrado(int numero);
+int cubo(int numero);
+
+int main(){
+    int numero;
+
+    printf("Informe um número: ");
+    scanf("%d", &numero);
+
+    printf("O quadrado desse número é: %d\nO cubo desse número é: %d", quadrado(numero), cubo(numero));
+}
+
+int quadrado(int numero){
+return numero*numero;
+}
+
+int cubo(int numero){
+return numero*numero*numero;
+}
