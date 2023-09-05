e3a1f22f9d122f1641df816b529c9949221a6daa
Alex Silva <106414021+httplex@users.noreply.github.com>
2023-09-05 04:43:18 +0000

    ok

---
+++ listaC/ex9.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int numero, cont = numero*cont;
+    printf("Informe um número: ");
+    scanf("%d", &numero);
+
+    for(int cont = 1; cont <=10; cont++){
+        printf("%dx%d = %d \n", cont, numero, cont * numero);
+        
+    }
+}
