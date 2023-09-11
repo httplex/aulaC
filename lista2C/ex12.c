12d4ce9aa278dfd3f0dc00af2090529005f2c21b
Alex Silva <106414021+httplex@users.noreply.github.com>
2023-09-11 23:17:35 +0000

    ok

--- lista2C/ex12.c
+++ lista2C/ex12.c
@@ -6,5 +6,16 @@ a soma dos dois anteriores.  */
 #include <stdlib.h>
 
 int main(){
-    
+    int termo1 = 0, termo2 = 1, somaTermos = 1;
+    int limite;
+
+    printf("Informe um número para que a sequência Fibonacci seja exibida: ");
+    scanf("%d", &limite);
+
+    while(somaTermos <= limite){
+        printf("%d\n", somaTermos);
+        termo1 = termo2;
+        termo2 = somaTermos;
+        somaTermos = termo1 + termo2;
+    }
 }
