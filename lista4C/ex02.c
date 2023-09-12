24a9c353bd6439c3caf91c087e8a023cbe27116a
Alex Silva <106414021+httplex@users.noreply.github.com>
2023-09-12 04:21:58 +0000

    ok

---
+++ lista4C/ex02.c
@@ -0,0 +1,5 @@
+/*  2. Implemente uma função que verifica se uma senha fornecida pelo usuário é válida. A
+senha deve ter pelo menos 8 caracteres e conter pelo menos uma letra maiúscula,
+uma letra minúscula e um número. A função deve retornar 1 se a senha for válida e 0
+caso contrário. Estudem a biblioteca ctype.h que contém funções para ajudar nesse
+exercício  */
